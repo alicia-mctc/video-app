5c46aed594ebe5aef629ee37759ff0091913d3a1
Alicia <alicia.nguyen@go.minneapolis.edu>
2020-11-24 23:55:29 -0600

    Modified video app

--- video_collection/models.py
+++ video_collection/models.py
@@ -1,12 +1,40 @@
+from urllib import parse
 from django.db import models
+from django.core.exception import ValidationError
 
 # Create your models here.
 class Video(models.Model):
     name = models.CharField(max_length=200)
     url = models.CharField(max_length=400)
     notes = models.TextField(blank=True, null=True)
+    video_id = models.CharField(max_length=40, unique=True)
+    
+    def save(self, *args, **kwargs):
+        #extract the video id from a youtube url 
+        # https://www.youtube.com/watch?v12345678
+        # where 12345678 is the video ID
+        #extract the video id from a youtube url, prevent save if not valid YouTube URL
+        #  or id ID is not found in URL
+
+        try: 
+            url_components = parse.urlparse(self.url)
+            if url_components.scheme != 'http' or url_components.netloc != 'www.youtube.com' or url_components.path != 'watch':
+                raise ValidationError(f'Invalid YouTube URL {self.url}')
+            
+            query_string = url_components.query
+            if not query_string:
+                raise ValidationError(f'Invalid YouTube URL {self.url}')
+            parameters = parse.parse_qs(query_string, strict_parsing=True)
+            parameter_list = parameters.get('v')
+            if not parameter_list: # empty string, empty list ...
+                raise ValidationError(f'Invalid YouTube URL parameters {self.url}')
+            self.video_id = parameter_list[0] # set the video ID for this Video object
+        except ValueError as e: # URL parsing errors, malformed URLs
+            raise ValidationError(f'Unable to parse URL {self.url}') from e
+
+        super().save(*args, **kwargs) # don't forget!
 
     def __str__(self):
         # String displayed in the admin console, or when printing a model object.
         # You can return any useful string here. Suggest truncating notes to max 200 characters.
-        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Video ID: {self.video.id}, Notes: {self.notes[:200]}'
--- video_collection/views.py
+++ video_collection/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from .models import Video
+from .forms import VideoForm
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
     app_name = 'Exercise Videos'
     return render(request, 'video_collection/home.html', { 'app_name': app_name})
+
+def add(request):
+    if request.method == 'POST': # adding a new video
+        new_video_form = VideoForm(request.POST)
+        if new_video_form.is_valid():
+            new_video_form.save() # Creates new Video object and saves
+            messages.info(request, 'New video saved!')
+            # todo redirect to list of videos
+        else:
+            messages.warning(request, 'Check the data entered')
+            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+def video_list(request):
+    videos = Video.objects.order_by('name')
+    return render(request, 'video_collection/video_list.html', {'videos': videos})
