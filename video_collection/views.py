d8e45f831182bd56a19e060c6b3da39c3464fb0b
Alicia <alicia.nguyen@go.minneapolis.edu>
2020-11-25 00:19:58 -0600

    Modify app

--- video_collection/views.py
+++ video_collection/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Video
 from .forms import VideoForm
 from django.contrib import messages
@@ -13,15 +13,21 @@ def add(request):
     if request.method == 'POST': # adding a new video
         new_video_form = VideoForm(request.POST)
         if new_video_form.is_valid():
-            new_video_form.save() # Creates new Video object and saves
-            messages.info(request, 'New video saved!')
-            # todo redirect to list of videos
-        else:
-            messages.warning(request, 'Check the data entered')
-            return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+            try:
+                new_video_form.save() # Creates new Video object and saves
+                return redirect('video_list')
+                # todo redirect to list of videos
+            except ValidationError:
+                messages.warning(request, 'Invalid YouTube URL')
+            except IntegrityError:
+                messages.warning(request, 'You already added that video')
+        
+        messages.warning(request, 'Check the data entered')
+        return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
 
     new_video_form = VideoForm()
     return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
 
 def video_list(request):
     videos = Video.objects.order_by('name')
